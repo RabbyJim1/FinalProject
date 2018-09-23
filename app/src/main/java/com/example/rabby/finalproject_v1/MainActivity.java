0cc2996ce2ef082a116a75823dc0dc3324350fa9
Md. Golam Rabby Jim <jim15-5551@diu.bd>
2018-09-24 02:17:17 +0600

    Initial commit 1. Profile Page Design - V1

---
+++ app/src/main/java/com/example/rabby/finalproject_v1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.rabby.finalproject_v1;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.proImg);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.jim);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        roundedBitmapDrawable.setCircular(true);
+        imageView.setImageDrawable(roundedBitmapDrawable);
+
+    }
+}
